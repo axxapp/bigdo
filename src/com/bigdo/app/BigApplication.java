c8ad83b264dab4c02183fc81c524da7173c328a3
aixiaoxianggithub <aixiaoxiang2009@hotmail.com>
2014-08-12 21:49:45 +0800

    开发主页

---
+++ src/com/bigdo/app/BigApplication.java
@@ -0,0 +1,7 @@
+package com.bigdo.app;
+
+import android.app.Application;
+
+public class BigApplication extends Application {
+
+}
--- src/com/bigdo/app/Main_Activity.java
+++ src/com/bigdo/app/Main_Activity.java
@@ -1,7 +1,26 @@
 package com.bigdo.app;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.widget.ImageView;
 
 public class Main_Activity extends Activity {
+	private final int waitime = 1000 * 10;
+	ImageView splash_log;
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.main_activity);
+		splash_log = (ImageView) findViewById(R.id.splash_log_main_activity);
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				splash_log.setVisibility(ImageView.GONE);
+			}
+		}, waitime);
+	}
 }
